c2bf4852acb0d8280fcb1b3d5519bdbd60e1581c
unknown <Forgeq>
2018-09-09 22:47:43 +0300

    Initial commit

---
+++ src/com/example/ger/privet.java
@@ -0,0 +1,8 @@
+package com.example.ger;
+
+public class privet {
+    public static void main(String[] arqs) {
+        System.out.println("\nКИШКА Б.В. \nГруппа: АТ-161");
+        System.out.println("\n привет");
+    }
+}
